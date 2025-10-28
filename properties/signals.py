5d2d0214d58094e28df2b84684b7b9a696d13fc0
Solomon Tchaya <tchayasolomon2000@gmail.com>
2025-10-28 23:27:03 +0200

    Week 10

---
+++ properties/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.core.cache import cache
+from .models import Property
+
+@receiver(post_save, sender=Property)
+def invalidate_cache_on_save(sender, instance, **kwargs):
+    """Invalidate cache when a Property is created or updated."""
+    cache.delete('all_properties')
+
+@receiver(post_delete, sender=Property)
+def invalidate_cache_on_delete(sender, instance, **kwargs):
+    """Invalidate cache when a Property is deleted."""
+    cache.delete('all_properties')
