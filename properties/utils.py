0baabab82270accc88dd510ee9e36eeb05676b51
Solomon Tchaya <tchayasolomon2000@gmail.com>
2025-10-28 23:31:29 +0200

    Week 10

--- properties/utils.py
+++ properties/utils.py
@@ -32,8 +32,8 @@ def get_redis_cache_metrics():
         misses = info.get("keyspace_misses", 0)
 
         # Avoid division by zero
-        total = hits + misses
-        hit_ratio = (hits / total) if total > 0 else 0
+        total_requests = hits + misses
+        hit_ratio = hits / total_requests if total_requests > 0 else 0
 
         metrics = {
             "hits": hits,
